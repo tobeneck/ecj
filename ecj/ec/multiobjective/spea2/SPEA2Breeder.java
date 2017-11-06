b9350d4a930fc2e1544fd42331eee6918b872e75
SigmaX <fyedernoggersnodden@gmail.com>
2017-11-06 14:48:09 -0500

    Revert "nsga2 fixes"
    This reverts commit 0e2f22c6d77212023808fb36c75277c85fe5fc12.

--- ecj/ec/multiobjective/spea2/SPEA2Breeder.java
+++ ecj/ec/multiobjective/spea2/SPEA2Breeder.java
@@ -30,6 +30,10 @@ public class SPEA2Breeder extends SimpleBreeder
     public void setup(final EvolutionState state, final Parameter base)
         {
         super.setup(state, base);
+
+        for (int i = 0; i < state.population.subpops.size(); i++)
+            if (reduceBy[i] != 0)
+                state.output.fatal("SPEA2Breeder does not support population reduction.", base.push(P_REDUCE_BY).push(""+i), null);
                         
         if (sequentialBreeding) // uh oh, haven't tested with this
             state.output.fatal("SPEA2Breeder does not support sequential evaluation.",
